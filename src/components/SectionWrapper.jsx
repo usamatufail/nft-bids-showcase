3ea683459c33094902e1ed319cd4cf2a446f3881
Usama <miansamusama@gmail.com>
2022-05-25 04:25:08 +0500

    feat(section-wrapper): changed link to nft place

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,41 +4,64 @@ import styles from '../styles/Global';
 import assets from '../assets';
 import Button from './Button';
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner, reverse }) => {
+const SectionWrapper = ({
+  title,
+  description,
+  showBtn,
+  mockupImg,
+  banner,
+  reverse,
+}) => {
   return (
-    <div className={`min-h-screen ${styles.section} 
-      ${reverse ? styles.bgWhite : styles.bgPrimary} 
-      ${banner}`}>
-      <div className={`flex items-center 
-        ${reverse ? styles.boxReverseClass : styles.boxClass} 
-        w-11/12 sm:w-full minmd:w-3/4`}>
-        <div className={`${styles.descDiv} 
-          ${reverse ? " fadeRightMini" : " fadeLeftMini"}
+    <div
+      className={`min-h-screen ${styles.section}
+      ${reverse ? styles.bgWhite : styles.bgPrimary}
+      ${banner}`}
+    >
+      <div
+        className={`flex items-center
+        ${reverse ? styles.boxReverseClass : styles.boxClass}
+        w-11/12 sm:w-full minmd:w-3/4`}
+      >
+        <div
+          className={`${styles.descDiv}
+          ${reverse ? ' fadeRightMini' : ' fadeLeftMini'}
           ${reverse ? styles.textRight : styles.textLeft}
-        `}>
-          <h1 className={`
+        `}
+        >
+          <h1
+            className={`
           ${reverse ? styles.blackText : styles.whiteText}
-          ${styles.h1Text}`}>{title}</h1>
-          <p className={`
+          ${styles.h1Text}`}
+          >
+            {title}
+          </h1>
+          <p
+            className={`
           ${reverse ? styles.blackText : styles.whiteText}
-          ${styles.descriptionText}`}>{description}</p>
+          ${styles.descriptionText}`}
+          >
+            {description}
+          </p>
           {showBtn && (
-            <Button 
+            <Button
               assetUrl={assets.expo}
-              link="https://expo.dev/@adrianhajdin/react_native_app?serviceType=classic&distribution=expo-go"
+              link="https://expo.dev/@samtufail726/nft_bids?serviceType=classic&distribution=expo-go"
             />
           )}
         </div>
-        <div
-          className={`flex-1 ${styles.flexCenter}p-8 sm:px-0`}
-        >
-          <img src={mockupImg} alt="mockup" className={`
-           ${reverse ? " fadeLeftMini" : " fadeRightMini"}
-          ${styles.sectionImg}`} />
+        <div className={`flex-1 ${styles.flexCenter}p-8 sm:px-0`}>
+          <img
+            src={mockupImg}
+            alt="mockup"
+            className={`
+           ${reverse ? ' fadeLeftMini' : ' fadeRightMini'}
+          ${styles.sectionImg}`}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper
+export default SectionWrapper;
